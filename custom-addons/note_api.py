0108ced03d13a65ab0af87f795fa03913c822336
joanDeveloper <joanmodaw@gmail.com>
2018-12-27 14:01:16 +0100

    webservice (note_api) + fixed bug tag

--- custom-addons/note_api.py
+++ custom-addons/note_api.py
@@ -1,33 +1,61 @@
 import xmlrpclib
 from pprint import pprint
 
-url = 'http://localhost:8069'
-db = 'v8dev'
-username = 'admin'
-password = 'admin'
+#url = 'http://localhost:8069'
+#db = 'v8dev'
+#username = 'admin'
+#password = 'admin'
 
-common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
-uid = common.authenticate(db, username, password, {})
+#common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
+#uid = common.authenticate(db, username, password, {})
 
-models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url))
+#models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url))
 
-search = models.execute_kw(db, uid, password,
-    'res.partner', 'search',
-    [[['is_company', '=', True], ['customer', '=', True]]])
+#search = models.execute_kw(db, uid, password,
+#    'res.partner', 'search',
+#    [[['is_company', '=', True], ['customer', '=', True]]])
 
-searchPag = models.execute_kw(db, uid, password,
-    'res.partner', 'search',
-    [[['is_company', '=', True], ['customer', '=', True]]],
-    {'offset': 10, 'limit': 5})
+#searchPag = models.execute_kw(db, uid, password,
+#    'res.partner', 'search',
+#    [[['is_company', '=', True], ['customer', '=', True]]],
+#    {'offset': 10, 'limit': 5})
 
-#models.execute_kw(db, uid, password, 'res.partner', 'write', [[80], {
- # 'name': "Newer partner"
-#}])
+#pprint(common.version())
+#pprint(uid)
+#pprint(search)
+#pprint(searchPag)
 
-#g = models.execute_kw(db, uid, password, 'res.partner', 'name_get', [[80]])
+import xmlrpclib
+class NoteAPI():
+    def __init__(self, srv, db, user, pwd):
+        common = xmlrpclib.ServerProxy(
+        '%s/xmlrpc/2/common' % srv)
+        self.api = xmlrpclib.ServerProxy(
+        '%s/xmlrpc/2/object' % srv)
+        self.uid = common.authenticate(db, user, pwd, {})
+        self.pwd = pwd
+        self.db = db
+        self.model = 'todo.task'
 
-pprint(common.version())
-pprint(uid)
-pprint(search)
-pprint(searchPag)
-#pprint(g)
+    def get(self, ids=None):
+        domain = [('id',' in', ids)] if ids else []
+        fields = ['id', 'name']
+        return self.execute('search_read', [domain, fields])
+
+    def execute(self, method, arg_list, kwarg_dict=None):
+        return self.api.execute_kw(
+        self.db, self.uid, self.pwd, self.model,
+        method, arg_list, kwarg_dict or {})
+
+    def set(self, text, id=None):
+        if id:
+            self.execute('write', [[id], {'name': text}])
+        else:
+            vals = {'name': text, 'user_id': self.uid}
+            id = self.execute('create', [vals])
+    
+if __name__ == '__main__':
+    srv, db = 'http://localhost:8069', 'v8dev'
+    user, pwd = 'admin', 'admin'
+    api = NoteAPI(srv, db, user, pwd)
+    pprint(api.get())
